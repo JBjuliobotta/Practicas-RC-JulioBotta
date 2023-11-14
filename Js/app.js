1fd258d80c16b6f688e076834c94e0a2dbad423c
juliobotta <jrb12494@gmail.com>
2023-11-14 05:58:52 -0300

    ejercicos de js del 1 al 3

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+alert('Un mensaje')
+
+let mensaje="Hola Mundo"
+document.write(mensaje);
+
+let numero_1=3;
+let numero_2=5;
+console.log(numero_1+numero_2);
+document.write('<br> El resultado de 3+5 es ' + (numero_1+numero_2))
