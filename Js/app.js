6a75d437121a0b904c11ca9dac0b5acc194fddaf
juliobotta <jrb12494@gmail.com>
2023-11-14 15:55:47 -0300

    ejercicios del 3 al 5 completados

--- Js/app.js
+++ Js/app.js
@@ -3,7 +3,18 @@ alert('Un mensaje')
 let mensaje="Hola Mundo"
 document.write(mensaje);
 
-let numero_1=3;
-let numero_2=5;
-console.log(numero_1+numero_2);
-document.write('<br> El resultado de 3+5 es ' + (numero_1+numero_2))
+let numero_1=3
+let numero_2=5
+document.write('<br> El resultado de 3+5 es ' + (numero_1+numero_2));
+
+let nombre
+nombre=prompt('Ingrese nombre Usuario',nombre);
+document.write('<br> Hola ' + nombre);
+
+let numeroUno
+let numeroDos
+
+numeroUno=parseInt(prompt('Ingrese el primer número'));
+numeroDos=parseInt(prompt('Ingrese el segundo número'));
+let suma=parseInt(numeroUno) + parseInt(numeroDos);
+document.write('<br> La suma de ambos números es igual a ' + suma);
