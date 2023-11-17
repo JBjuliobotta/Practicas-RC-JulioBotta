8488fc9fcceeff5bf99407a470c114d268f26074
juliobotta <jrb12494@gmail.com>
2023-11-17 20:58:23 -0300

    realizado algunos ejercicios de switch

---
+++ js/switch.js
@@ -0,0 +1,98 @@
+/*1)
+let mes;
+mes=prompt('Ingrese un numero del 1 al 12');
+
+switch(mes) {
+    case '1':
+    document.write('Enero');
+    break;
+    case '2':
+    document.write('Febrero');
+    break;
+    case '3':
+    document.write('Marzo');
+    break;
+    case '4':
+    document.write('Abril');
+    break;
+    case '5':
+    document.write('Mayo');
+    break;
+    case '6':
+    document.write('Junio');
+    break;
+    case '7':
+    document.write('Julio');
+    break;
+    case '8':
+    document.write('Agosto');
+    break;
+    case '9':
+    document.write('Septiembre');
+    break;
+    case '10':
+    document.write('Octubre');
+    break;
+    case '11':
+    document.write('Noviembre');
+    break;
+    case '12':
+    document.write('Diciembre');
+    break;
+
+}*/
+
+/*2)
+let dia;
+dia=prompt('Ingrese un numero del 1 al 7');
+
+switch(dia) {
+    case '1':
+    document.write('Domingo');
+    break;
+    case '2':
+    document.write('Lunes');
+    break;
+    case '3':
+    document.write('Martes');
+    break;
+    case '4':
+    document.write('Miercoles');
+    break;
+    case '5':
+    document.write('Jueves');
+    break;
+    case '6':
+    document.write('Viernes');
+    break;
+    case '7':
+    document.write('Sabado');
+    break;
+}*/
+
+/*3)
+let letra;
+letra=prompt('Ingrese una letra cualquiera');
+
+switch(letra) {
+    case 'a' :
+    document.write('Es una vocal');
+    break;
+    case 'e' :
+    document.write('Es una vocal');
+    break;
+    case 'i' :
+    document.write('Es una vocal');
+    break;
+    case 'o' :
+    document.write('Es una vocal');
+    break;
+    case 'u' :
+    document.write('Es una vocal');
+    break;
+    default :
+    document.write('No es una vocal');
+    break;
+}*/
+
+/*4)*/
