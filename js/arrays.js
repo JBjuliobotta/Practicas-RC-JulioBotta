c0728f2c51c968830797221e317a7679a5ce21e3
Julio Rafael Botta <125596633+JBjuliobotta@users.noreply.github.com>
2023-11-29 18:00:20 +0000

    subiendo cambios estancados de la previa sesion por fallas tecnicas

---
+++ js/arrays.js
@@ -0,0 +1,3 @@
+// Dado un array de números, escribir una función que retorne el número más grande del array.
+let arr=[1, 20, 155, 45, 200, 44, 4];
+console.log(Math.max(...arr));
---
+++ js/ciclos.js
@@ -0,0 +1,7 @@
+/* Imprimir los números pares del 2 al 20 en la consola.
+let i=0;
+while (i<=20) {
+   console.log(i);
+   i= i + 2;
+}*/
+
